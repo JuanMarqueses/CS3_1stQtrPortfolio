57bfb7295d74bb8825c2db5c503998da847446ce
JuanMarqueses <b2029jlcmarqueses@pshs.edu.ph>
2025-08-05 08:47:11 +0800

    First NG

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-//no need for script tags here
+// no script tag needed here
+
+let age = 15
+let maxAge = 100
+let numPerDay = 2
+totalRequired = (numPerDay * 365) * (maxAge - age); 
+
+document.write("You will need " + totalRequired + " buckets of fried chicken to last you until the ripe old age of " + maxAge);
