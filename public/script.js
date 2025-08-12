a16f49007bfce2d147633d37f6d300e997269411
JuanMarqueses <b2029jlcmarqueses@pshs.edu.ph>
2025-08-12 08:52:23 +0800

    2nd Speed Test

--- public/script.js
+++ public/script.js
@@ -1,13 +1,16 @@
 // no script tag needed here
 
+
 let age = window.prompt("Enter your age ")
 let maxAge = window.prompt("Enter your Max age ")
 let numPerDay = window.prompt("Enter your Number Per day ")
 
 totalRequired = (numPerDay * 365) * (maxAge - age); 
 
-if (confirm(`IS everything correct?`)){
+if (confirm(`Is everything correct?`)){
     console.log(document.write("You will need " + totalRequired + " buckets of fried chicken to last you until the ripe old age of " + maxAge))
 }else{
     document.write("Run the code again :D");
 }
+</script>
+</body>
